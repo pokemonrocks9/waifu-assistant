2d3b8bbb35f49ce1cf6c36840bfa32c49223d68e
pokemonrocks9 <tytygoins@gmail.com>
2026-07-17 15:45:36 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for Compa PWA
 // Bump this version number on every deploy to force clients to pick up new files
-const SW_VERSION = '0.4.2';
+const SW_VERSION = '0.5.0';
 const CACHE_NAME = `compa-pod-${SW_VERSION}`;
 const urlsToCache = [
   './',

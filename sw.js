04f3e844e274e305bfd66b4dfad94a4023aeecfe
pokemonrocks9 <tytygoins@gmail.com>
2026-07-20 11:20:26 -0400

    Update service worker version to 0.6.0

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for Compa PWA
 // Bump this version number on every deploy to force clients to pick up new files
-const SW_VERSION = '0.5.6';
+const SW_VERSION = '0.6.0';
 const CACHE_NAME = `compa-pod-${SW_VERSION}`;
 const urlsToCache = [
   './',

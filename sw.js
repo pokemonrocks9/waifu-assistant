49216c98727e4eb213ca06d3e32852a3660f396e
pokemonrocks9 <tytygoins@gmail.com>
2026-07-20 22:33:57 -0400

    Bump service worker version to 0.6.11

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for Compa PWA
 // Bump this version number on every deploy to force clients to pick up new files
-const SW_VERSION = '0.6.10';
+const SW_VERSION = '0.6.11';
 const CACHE_NAME = `cpu-pod-${SW_VERSION}`;
 const urlsToCache = [
   './',
